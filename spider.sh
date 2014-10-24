03901799c3b310014824cf165587ebb66e042829
bedreamer <bedreamer@163.com>
2014-10-24 15:12:19 +0800

    add spider domain support

--- spider.sh
+++ spider.sh
@@ -3,6 +3,8 @@
 DOMAIN=$1
 DATABASE=$DOMAIN.log
 g_urls=
+CACHEDIR=/run/shm/
+
 # 将制定页面中本站的链接找出来并返回
 function spider_page() {
 	thiz=`curl $1 2>/dev/null|grep -o "href=[\'\"]http://[^\"\'\?\#]\+[\"]"|grep -o "http://[^\"]\+"|uniq 2>/dev/null`
@@ -24,17 +26,48 @@ function spider_page() {
 	done
 }
 
-function spider_enum() {
+function spider_domain() {
+	thiz=`curl $1 2>/dev/null|grep -o "href=[\'\"]http://[^\"\'\?\#]\+[\"]"|grep -o "http://[^\"\/]\+"|sed 's/http:\/\///' | sort|uniq 2>/dev/null`
+	g_urls="";
+	for u in $thiz; do
+		grep $u $DATABASE >/dev/null 2>&1
+		if [ $? == 0 ]; then
+		#	echo exist URL: $u
+			continue; 
+		fi
+		#echo $u | grep $DOMAIN >/dev/null 2>&1
+		#if [ $? != 0 ]; then 
+		#	echo not domain URL: $u
+		#	continue; 
+		#fi
+		g_urls="$g_urls $u";
+		echo $u >> $DATABASE
+		echo "   new domain: $u"
+	done
+}
+
+function spider_page_enum() {
 	for u in $@; do
 		if [ ${#u} == 0 ];then continue; fi
 		echo spidering on page: $u
 		spider_page $u
 		newurls="$newurls $g_urls"
 	done
-	spider_enum $newurls
+	spider_page_enum $newurls
 }
 
-spider_page $DOMAIN
+function spider_domain_enum() {
+	for u in $@; do
+		if [ ${#u} == 0 ];then continue; fi
+		echo spidering on page: $u
+		spider_domain $u
+		newurls="$newurls $g_urls"
+	done
+	spider_domain_enum $newurls
+}
+
+
+spider_domain $DOMAIN
 urls=$g_urls
 #echo $urls
 for u in $urls; do
@@ -44,5 +77,5 @@ for u in $urls; do
 	echo "   new url: $u"
 done
 
-spider_enum $urls
+spider_domain_enum $urls
 echo "done"
