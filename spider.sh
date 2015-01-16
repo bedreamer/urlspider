7f40e9f1302ff874b2c9b99a703fa9949bd76886
bedreamer <bedreamer@163.com>
2015-01-16 15:59:43 +0800

    从数据库缓冲没有遍历的地址，并把新的地址不断的添加到数据库中

--- spider.sh
+++ spider.sh
@@ -2,8 +2,10 @@
 
 DOMAIN=$1
 DATABASE=$DOMAIN.log
+DB=db.db
 g_urls=
 CACHEDIR=/run/shm/
+urlnr=
 
 # 将制定页面中本站的链接找出来并返回
 function spider_page() {
@@ -23,11 +25,37 @@ function spider_page() {
 		g_urls="$g_urls $u";
 		echo $u >> $DATABASE
 		echo "   new url: $u"
+		./a main.db "insert into links values('$u','dfadf');"
+	done
+}
+
+# 使用数据库作为缓冲区 
+function spider_page_db() {
+	thiz=`curl $1 2>/dev/null|grep -o "href=[\'\"]http://[^\"\'\?\#]\+[\"]"|grep -o "http://[^\"]\+"|uniq 2>/dev/null`
+	for u in $thiz; do
+		#echo "SELECT * FROM urls WHERE url='$u'"
+		u_md5a=`echo "$u" | md5sum | awk '{print $1}'`
+		u_md5b=`echo "$u/" | md5sum | awk '{print $1}'`
+		result=`./db $DB "SELECT md5 FROM urls WHERE md5='$u_md5a' OR md5='$u_md5b'"`
+		if [ ${#result} != 0 ]; then
+		#	echo exist URL: $u
+		#	echo "-------REPEATE------ $result:::${#result}:::$u"
+			continue; 
+		fi
+		#echo $u | grep $DOMAIN >/dev/null 2>&1
+		#if [ $? != 0 ]; then 
+		#	echo not domain URL: $u
+		#	continue; 
+		#fi
+		nr=`./db $DB "SELECT COUNT(*)+1 FROM urls"`
+		echo "[ $nr ] $u_md5a:$u"
+		./db $DB "INSERT INTO urls VALUES('$nr','$u_md5a','$u');"
 	done
 }
 
 function spider_domain() {
-	thiz=`curl $1 2>/dev/null|grep -o "href=[\'\"]http://[^\"\'\?\#]\+[\"]"|grep -o "http://[^\"\/]\+"|sed 's/http:\/\///' | sort|uniq 2>/dev/null`
+	echo "www.$1"
+	thiz=`curl www.$1 2>/dev/null|grep -o "href=[\'\"]http://[^\"\'\?\#]\+[\"]"|grep -o "http://[^\"\/]\+"|sed 's/http:\/\///'|grep -o '[^\.\/\?\#]\+\.com[^\/\?\#]\+' | sort|uniq 2>/dev/null`
 	g_urls="";
 	for u in $thiz; do
 		grep $u $DATABASE >/dev/null 2>&1
@@ -42,7 +70,13 @@ function spider_domain() {
 		#fi
 		g_urls="$g_urls $u";
 		echo $u >> $DATABASE
-		echo "   new domain: $u"
+		geo=`geoiplookup $u`;
+		echo $geo | grep "can\'t" >/dev/null 2>&1
+		if [ $? == 0 ]; then
+			echo "   new domain: $u   -->  $geo"
+		else
+			echo "   new domain: $u   -->  N/A"
+		fi
 	done
 }
 
@@ -66,16 +100,21 @@ function spider_domain_enum() {
 	spider_domain_enum $newurls
 }
 
+if [ -e $DB ]; then
+	rm $DB;
+fi
+./db $DB "create table urls(id unsigned int,md5 string,url string);"
+md=`echo '$1' | md5sum | awk '{print $1}'` 
+./db $DB "INSERT INTO urls VALUES('0', '$md', '$1');"
 
-spider_domain $DOMAIN
-urls=$g_urls
-#echo $urls
-for u in $urls; do
-	#echo $u | grep $DOMAIN > /dev/null 2>&1
-	#if [ $? != 0 ]; then continue; fi
-	echo $u >> $DATABASE
-	echo "   new url: $u"
-done
-
-spider_domain_enum $urls
-echo "done"
+i=0
+while [ $i -ge 0 ]
+	do
+		sql="SELECT url FROM urls WHERE id='$i'";
+		url=`./db $DB "$sql"`
+		echo "spider url $sql ==> $url"
+		spider_page_db $url
+		echo "--------->"
+		i=$(($i+1))
+	done
+echo done.
